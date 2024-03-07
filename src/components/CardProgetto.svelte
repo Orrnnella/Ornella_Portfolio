049b2acacca74dfd71bc4cc5342f522447078eb8
Orrnnella <162459707+Orrnnella@users.noreply.github.com>
2024-03-07 18:43:34 +0100

    Modifiche stilistiche

--- src/components/CardProgetto.svelte
+++ src/components/CardProgetto.svelte
@@ -6,79 +6,75 @@
 	export let anno;
 </script>
 
-
 <svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://unpkg.com/tailwindcss@3.4.1/src/css/preflight.css"
-	/>
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@3.4.1/src/css/preflight.css" />
 </svelte:head>
-
 
 <div class="Card">
 	<div class="categorie">
-	{#each categorie as categoria,i}
-		<p
-			class="categoria"
-			class:categoria-bold={i==0}
-		>
-			{categoria}
-		</p>
-	{/each}
+		{#each categorie as categoria, i}
+			<p class="categoria" class:categoria-bold={i == 0}>
+				{categoria}
+			</p>
+		{/each}
 	</div>
-	<img class="image" src={image} alt="cover_progetto"/>
+	<img class="image" src={image} alt="cover_progetto" />
 	<p class="titolo">{titolo}</p>
 	<div class="footer">
-	<p class="tipologia">{tipologia}</p>
-	<p class="anno">{anno}</p>
+		<p class="tipologia">{tipologia}</p>
+		<p class="anno">{anno}</p>
 	</div>
 </div>
 
 <style>
- .Card{
-	background-color: black;
-	border-radius:20px;
-	color: white;
-	font-family: helvetica;
-	padding: 20px;
-}
-	.categorie{
-	font-size:32px;
-	display:flex;
-	flex-direction:row;
-	justify-content:left;
-	gap:20px;
+	.Card {
+		background-color: black;
+		border-radius: 20px;
+		color: red;
+		font-family: helvetica;
+		padding: 20px;
+		margin-top: 20px;
+		margin-bottom: 20px;
+		margin-left: 10px;
+		margin-right: 10px;
+	}
+	.categorie {
+		font-size: 32px;
+		display: flex;
+		flex-direction: row;
+		justify-content: left;
+		gap: 20px;
 	}
 
-	.categoria{
-	border-color: white;
-	border-width:2px;
-	color:white;	
-	border-radius:60px;
-	padding:10px;
+	.categoria {
+		border-color: red;
+		border-width: 2px;
+		color: red;
+		border-radius: 60px;
+		padding: 10px;
 	}
 
 	.categoria-bold {
-	background-color:white;
-	color:black;
-	border-radius:60px;
-	padding:10px;
+		background-color: red;
+		color: black;
+		border-radius: 60px;
+		padding: 10px;
 	}
 
-	.image{
+	.image {
 		object-fit: cover;
 		aspect-ratio: 4/5;
-		width:100%;
+		width: 100%;
 		border-radius: 20px;
-		margin-top:20px;
-}
-	.titolo{
-		font-size:128px;		
+		margin-top: 20px;
 	}
-.footer{
-	display:flex;
-	flex-direction:row;
-	justify-content:space-between;
-	font-size:32px;
-}
+	.titolo {
+		font-size: 148px;
+	}
+	.footer {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		font-size: 32px;
+	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,49 @@
 <script>
-	import CardProgetto from "$components/CardProgetto.svelte";
+	import CardProgetto from '$components/CardProgetto.svelte';
 
-	let card_progetti=[
+	import image1 from './_images/Mbeh.jpg';
+	import image2 from './_images/Europeana-2.jpg';
+	import image4 from './_images/Macro.png';
+	import image5 from './_images/Is-it-a-zine.jpg';
+	let card_progetti = [
 		{
-		categorie:["Information Design", "Editorial", "UI"],
-		image:"https://images.unsplash.com/photo-1709626011485-6fe000ea2dbc?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-		titolo:"Europeana",
-		tipologia:"MA project",
-		anno:"2023"
-	},
+			categorie: ['Information Design', 'Editorial', 'UI'],
+			image: image2,
+			titolo: 'Europeana',
+			tipologia: 'MA project',
+			anno: '2023'
+		},
 		{
-		categorie:["Branding", "Wayfinding", "Typography"],
-		image:"https://images.unsplash.com/photo-1709626011485-6fe000ea2dbc?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-		titolo:"Macro",
-		tipologia:"MA project",
-		anno:"2023"
-	},
+			categorie: ['Branding', 'Wayfinding', 'Typography'],
+			image: image4,
+			titolo: 'Macro',
+			tipologia: 'MA project',
+			anno: '2023'
+		},
 		{
-		categorie:["Typography","Editorial"],
-		image:"https://images.unsplash.com/photo-1709626011485-6fe000ea2dbc?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-		titolo:"Mbeh",
-		tipologia:"MA project",
-		anno:"2023"
-	},
-		{
-		categorie:["Visual Design","Editorial"],
-		image:"https://images.unsplash.com/photo-1709626011485-6fe000ea2dbc?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-		titolo:"Cartastraccia",
-		tipologia:"MA project",
-		anno:"2023"
-	}
-	]
+			categorie: ['Typography', 'Editorial'],
+			image: image1,
+			titolo: 'Mbeh',
+			tipologia: 'MA project',
+			anno: '2023'
+		},
 
-
-
-	
+		{
+			categorie: ['Illustration', 'Editorial'],
+			image: image5,
+			titolo: 'Is it a zine?',
+			tipologia: 'BA thesis',
+			anno: '2022'
+		}
+	];
 </script>
 
 {#each card_progetti as progetto}
-
-<CardProgetto
-categorie={progetto.categorie}
-image={progetto.image}
-titolo={progetto.titolo}
-tipologia={progetto.tipologia}
-anno={progetto.anno}
+	<CardProgetto
+		categorie={progetto.categorie}
+		image={progetto.image}
+		titolo={progetto.titolo}
+		tipologia={progetto.tipologia}
+		anno={progetto.anno}
 	/>
 {/each}
